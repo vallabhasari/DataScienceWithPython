10e032902e1083bbfea01b0076d3d1d8a209b267
vallabhasari <68625335+vallabhasari@users.noreply.github.com>
2020-07-21 23:08:25 -0400

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,8 @@
+x = int(input("Enter a number \n"))
+print("The factors of",x,"are:")
+for i in range(1, x + 1):
+    if x % i == 0:
+        if i % 2 == 0:
+            print(i," Even")
+        else:
+            print(i," Odd")
